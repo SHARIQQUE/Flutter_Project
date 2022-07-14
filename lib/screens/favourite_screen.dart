9b76b2acf24f6c32c2496c6023f9acf41b16bda2
User <sharique9718@gmail.com>
2022-07-14 17:52:05 +0530

    Grocery APP

---
+++ lib/screens/favourite_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:grocery_app/common_widgets/app_text.dart';
+
+import '../common_widgets/app_button.dart';
+import '../styles/colors.dart';
+
+class FavouriteScreen extends StatelessWidget {
+  final String imagePath = "assets/images/welcome_image.png";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColors.primaryColor,
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Spacer(),
+                icon(),
+                SizedBox(
+                  height: 20,
+                ),
+                welcomeTextWidget(),
+                SizedBox(
+                  height: 10,
+                ),
+                sloganText(),
+                SizedBox(
+                  height: 40,
+                ),
+                SizedBox(
+                  height: 40,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget icon() {
+    String iconPath = "assets/icons/app_icon.svg";
+    return SvgPicture.asset(
+      iconPath,
+      width: 48,
+      height: 56,
+    );
+  }
+
+  Widget welcomeTextWidget() {
+    return Column(
+      children: [
+        AppText(
+          text: "Welcome",
+          fontSize: 48,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+        ),
+        AppText(
+          text: "to our fav",
+          fontSize: 48,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+        ),
+      ],
+    );
+  }
+
+  Widget sloganText() {
+    return AppText(
+      text: "",
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: Color(0xffFCFCFC).withOpacity(0.7),
+    );
+  }
+
+
+}
+
